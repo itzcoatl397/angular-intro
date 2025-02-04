cfcd3898878e1b79192b771e321c71efa269a1db
itzcoatl397 <GerardoItzcoatl.21@outlook.com>
2025-02-03 22:04:16 -0600

    Descripción clara de los cambios

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-dbz-main-page',
+    templateUrl: 'main-page.component.html',
+    styleUrl:'./main.css',
+    standalone:false,
+})
+
+export class MainPageComponent {
+
+}
